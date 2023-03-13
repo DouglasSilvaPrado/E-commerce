3b954b3bd5739039f1f2f77342e2c797bd050518
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2023-03-13 15:50:57 -0300

    feat: add static navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
+import { Navbar } from "./components/Navbar";
 import { AppRoutes } from "./routes/index.jsx";
+import styles from "./App.css";
 
 export const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <AppRoutes />
     </div>
   );
